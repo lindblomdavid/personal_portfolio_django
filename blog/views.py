bdc5ae3f38c057e6e5294119ca68084d928fac00
David Lindblom <daavidlindblom@gmail.com>
2022-05-03 15:01:34 +0200

    First commit
    First commit

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from .models import Blog
 
 def all_blogs(request):
-    blogs = Blog.objects.order_by('-date')[:5] #order by date and give the top 5
+    blogs = Blog.objects.order_by('-date')#[:5] #order by date and give the top 5
     return render(request, 'blog/all_blogs.html', {'blogs':blogs})
 
 
--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path, PurePath
 import os
+from personal_portfolio import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm%b3%*hyiai74atxgzk+p=9d)v56+ph1($)=1p-38mmsn+df_b'
+SECRET_KEY = key.skey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
